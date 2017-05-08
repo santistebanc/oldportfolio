7f1257f5d6091419bbd5c334801845a21f1f880f
Carlos Santisteban <Carlos Santisteban>
2017-05-07 19:38:40 -0500

    ejected and added markup loader

--- src/MyStory.js
+++ src/MyStory.js
@@ -4,20 +4,17 @@ import { Header, Divider, Container, Icon, Segment } from 'semantic-ui-react'
 
 import story from './story.md'
 
-console.log(story);
-
-// const converter = new showdown.Converter();
-
-const content = "";
+const content = <div dangerouslySetInnerHTML={{__html: story}} />;
 
 export default class MyStory extends Component {
 
   render() {
+
     return (
       <Container className="nomargin">
         <Segment>
           <br/>
-          <Container text>
+          <Container text style={{paddingLeft: 20, paddingRight: 20}}>
             {content}
           </Container>
           <br/>
